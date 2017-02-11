c24914d4c2aee30a51b00309ee0279297fdeae83
Brian K. Christensen <briankchristensen@gmail.com>
2017-02-12 00:40:21 +0100

    DAG: Implemeted a simple dependency graph data structure
    This is useful for implementing dependencies between validators
    and resolve them in "natural" resolutaion order.
    It will be used for cycle detection as well.
    
    Later on, I maybe will open up for cycle depencies in context of live
    validators

---
+++ src/__tests__/dependency-graph.ts
@@ -0,0 +1,424 @@
+import {
+    Graph,
+    GraphError,
+} from '../dependency-graph';
+
+class MockGraph<K, V> extends Graph<K, V> {
+    public getIncomingEdges() {
+        return this.incomingEdges;
+    }
+
+    public getOutgoingEdges() {
+        return this.outgoingEdges;
+    }
+
+    public getNodes(): Map<K, V> {
+        return this.nodes;
+    }
+}
+
+test('hasNode returns true for added node', () => {
+    const graph = new Graph<string, void>();
+    const node = 'node';
+    graph.addNode(node, undefined);
+    expect(graph.hasNode(node)).toBe(true);
+});
+
+test('getNodeData retuns data attached to the node via addNode', () => {
+    const graph = new Graph<string, number>();
+    const node = 'node';
+    graph.addNode(node, 1);
+    expect(graph.getNodeData(node)).toEqual(1);
+});
+
+test('getNodeData throws when node does not exist', () => {
+    const graph = new Graph<string, void>();
+    const node = 'node';
+    expect(() => graph.getNodeData(node)).toThrowError("Node 'node' does not exist in graph");
+});
+
+test('adding node that already exists is a no-op', () => {
+    const graph = new Graph<string, number>();
+    const node = 'node';
+    graph.addNode(node, 1);
+    graph.addNode(node, 2);
+    expect(graph.getNodeData(node)).toEqual(1);
+});
+
+test('hasNode returns false when node does not exist', () => {
+    const graph = new Graph<string, void>();
+    const node = 'node';
+    expect(graph.hasNode(node)).toBe(false);
+});
+
+test('setNodeData overwrites existing data on node', () => {
+    const graph = new Graph<string, number>();
+    const node = 'node';
+    graph.addNode(node, 1);
+    expect(graph.getNodeData(node)).toEqual(1);
+    graph.setNodeData(node, 2);
+    expect(graph.getNodeData(node)).toEqual(2);
+});
+
+test('setNodeData on node that does not exist throws', () => {
+    const graph = new Graph<string, number>();
+    const node = 'node';
+    expect(() => graph.setNodeData(node, 2)).toThrowError("Node 'node' does not exist in graph");
+});
+
+test('adding dependency adds incoming and outgoing edges', () => {
+    const graph = new MockGraph<string, number>();
+    const node1 = 'node1';
+    const node2 = 'node2';
+
+    graph.addNode(node1, 1);
+    graph.addNode(node2, 2);
+    graph.addDependency(node1, node2);
+
+    const incomingEdges = graph.getIncomingEdges();
+    const outgoingEdges = graph.getOutgoingEdges();
+
+    const node1IncomingEdges = incomingEdges.get(node1) as Set<string>;
+    const node1OutgoingEdges = outgoingEdges.get(node1) as Set<string>;
+    const node2IncomingEdges = incomingEdges.get(node2) as Set<string>;
+    const node2OutgoingEdges = outgoingEdges.get(node2) as Set<string>;
+
+    expect(node1IncomingEdges.size).toEqual(0);
+    expect(node1OutgoingEdges.size).toEqual(1);
+    expect(node2IncomingEdges.size).toEqual(1);
+    expect(node2OutgoingEdges.size).toEqual(0);
+    
+    expect(node1OutgoingEdges.has(node2)).toBe(true);
+    expect(node2IncomingEdges.has(node1)).toBe(true);
+});
+
+test('adding dependency where the "from" node does not exist throws', () => {
+    const graph = new Graph<string, number>();
+    const node1 = 'node1';
+    const node2 = 'node2';
+
+    graph.addNode(node2, 2);
+    expect(() => graph.addDependency(node1, node2)).toThrowError("Node 'node1' does not exist in graph");
+});
+
+test('adding dependency where "to" node does not exist throws', () => {
+    const graph = new Graph<string, number>();
+    const node1 = 'node1';
+    const node2 = 'node2';
+
+    graph.addNode(node1, 1);
+    expect(() => graph.addDependency(node1, node2)).toThrowError("Node 'node2' does not exist in graph");
+});
+
+test('adding a dependency that already exists is a no-op', () => {
+    const graph = new MockGraph<string, number>();
+    const node1 = 'node1';
+    const node2 = 'node2';
+
+    graph.addNode(node1, 1);
+    graph.addNode(node2, 2);
+
+    const node1OutgoingEdges = graph.getOutgoingEdges().get(node1) as Set<string>;
+    const node2IncomingEdges = graph.getIncomingEdges().get(node2) as Set<string>;
+
+    graph.addDependency(node1, node2);
+
+    expect(node1OutgoingEdges.size).toEqual(1);
+    expect(node2IncomingEdges.size).toEqual(1);
+    
+    graph.addDependency(node1, node2);
+
+    expect(node1OutgoingEdges.size).toEqual(1);
+    expect(node2IncomingEdges.size).toEqual(1);
+});
+
+test('removing a dependency removes incoming and outgoing edges', () => {
+    const graph = new MockGraph<string, number>();
+    const node1 = 'node1';
+    const node2 = 'node2';
+
+    graph.addNode(node1, 1);
+    graph.addNode(node2, 2);
+    
+    const node1OutgoingEdges = graph.getOutgoingEdges().get(node1) as Set<string>;
+    const node2IncomingEdges = graph.getIncomingEdges().get(node2) as Set<string>;
+
+    graph.addDependency(node1, node2);
+
+    expect(node1OutgoingEdges.size).toEqual(1);
+    expect(node2IncomingEdges.size).toEqual(1);
+
+    graph.removeDependency(node1, node2);
+
+    expect(node1OutgoingEdges.size).toEqual(0);
+    expect(node2IncomingEdges.size).toEqual(0);
+});
+
+test('removing a dependency that does not exist does not throw', () => {
+    const graph = new MockGraph<string, number>();
+    const node1 = 'node1';
+    const node2 = 'node2';
+
+    graph.addNode(node1, 1);
+    graph.addNode(node2, 2);
+
+    // reverse order
+    graph.addDependency(node2, node1);
+
+    const nodes = graph.getNodes();
+    
+    const outgoingEdges = graph.getOutgoingEdges().get(node1);
+    const incomingEdges = graph.getIncomingEdges().get(node2);
+
+    expect(nodes.size).toEqual(2);
+    expect(incomingEdges.size).toEqual(0);
+    expect(outgoingEdges.size).toEqual(0);
+
+    graph.removeDependency(node1, node2);
+
+    expect(incomingEdges.size).toEqual(0);
+    expect(outgoingEdges.size).toEqual(0);
+});
+
+test('dependenciesOf returns the sub tree of the node in question', () => {
+    const graph = new Graph<string, number>();
+    const a = 'A';
+    const b = 'B';
+    const c = 'C';
+    const d = 'D';
+    const e = 'E';
+    const f = 'F';
+    const g = 'G';
+
+    const data = 1;
+
+    graph.addNode(a, data);
+    graph.addNode(b, data);
+    graph.addNode(c, data);
+    graph.addNode(d, data);
+    graph.addNode(e, data);
+    graph.addNode(f, data);
+    graph.addNode(g, data);
+
+    //      A
+    //     / \
+    //    B   C
+    //   / \ / \
+    //  D  E F  G
+
+    graph.addDependency(a, b);
+    graph.addDependency(a, c);
+    graph.addDependency(b, d);
+    graph.addDependency(b, e);
+    graph.addDependency(c, f);
+    graph.addDependency(c, g);
+
+    expect(graph.dependenciesOf(g, false).size).toEqual(0);
+    expect(graph.dependenciesOf(f, false).size).toEqual(0);
+    expect(graph.dependenciesOf(e, false).size).toEqual(0);
+    expect(graph.dependenciesOf(d, false).size).toEqual(0);
+    expect(graph.dependenciesOf(c, false).size).toEqual(2);
+    expect(graph.dependenciesOf(b, false).size).toEqual(2);
+    expect(graph.dependenciesOf(a, false).size).toEqual(6);
+
+    expect(graph.dependenciesOf(a, true).size).toEqual(4);
+    expect(graph.dependenciesOf(b, true).size).toEqual(2);
+    expect(graph.dependenciesOf(c, true).size).toEqual(2);
+
+    expect(
+        Array.from(graph.dependenciesOf(a, false)).sort()
+    ).toEqual(
+        [b, c, d, e, f, g]
+    );
+
+    expect(
+        Array.from(graph.dependenciesOf(b, false)).sort()
+    ).toEqual(
+        [d, e]
+    );
+
+    expect(
+        Array.from(graph.dependenciesOf(c, false)).sort()
+    ).toEqual(
+        [f, g]
+    );
+});
+
+test('dependantsOf returns all nodes that are depending on the node itself', () => {
+    const graph = new Graph<string, number>();
+    const a = 'A';
+    const b = 'B';
+    const c = 'C';
+    const d = 'D';
+    const e = 'E';
+    const f = 'F';
+    const g = 'G';
+
+    const data = 1;
+
+    graph.addNode(a, data);
+    graph.addNode(b, data);
+    graph.addNode(c, data);
+    graph.addNode(d, data);
+    graph.addNode(e, data);
+    graph.addNode(f, data);
+    graph.addNode(g, data);
+
+    //      A
+    //     / \
+    //    B   C
+    //   / \ / \
+    //  D  E F  G
+
+    graph.addDependency(a, b);
+    graph.addDependency(a, c);
+    graph.addDependency(b, d);
+    graph.addDependency(b, e);
+    graph.addDependency(c, f);
+    graph.addDependency(c, g);
+
+    expect(graph.dependantsOf(g).size).toEqual(2);
+    expect(graph.dependantsOf(f).size).toEqual(2);
+    expect(graph.dependantsOf(e).size).toEqual(2);
+    expect(graph.dependantsOf(d).size).toEqual(2);
+    expect(graph.dependantsOf(c).size).toEqual(1);
+    expect(graph.dependantsOf(b).size).toEqual(1);
+    expect(graph.dependantsOf(a).size).toEqual(0);
+
+    expect(
+        Array.from(graph.dependantsOf(g)).sort()
+    ).toEqual([a, c]);
+
+    expect(
+        Array.from(graph.dependantsOf(f)).sort()
+    ).toEqual([a, c]);
+
+    expect(
+        Array.from(graph.dependantsOf(e)).sort()
+    ).toEqual([a, b]);
+
+    expect(
+        Array.from(graph.dependantsOf(d)).sort()
+    ).toEqual([a, b]);
+
+    expect(
+        Array.from(graph.dependantsOf(c))
+    ).toEqual([a]);
+
+    expect(
+        Array.from(graph.dependantsOf(b))
+    ).toEqual([a]);
+
+    expect(
+        Array.from(graph.dependantsOf(a))
+    ).toEqual([]);
+});
+
+test('resolving dependencies of a node that does not exist throws', () => {
+    const graph = new Graph<string, number>();
+    const node = 'node';
+    expect(() => graph.dependenciesOf(node)).toThrowError("Node 'node' does not exist in graph");
+});
+
+test('resolving dependants of a node that does not exist throws', () => {
+    const graph = new Graph<string, number>();
+    const node = 'node';
+    expect(() => graph.dependantsOf(node)).toThrowError("Node 'node' does not exist in graph");
+});
+
+test('resolving dependencies in a graph that has cycle(s) throws', () => {
+    const graph = new Graph<string, void>();
+    const a = 'A';
+    const b = 'B';
+    const c = 'C';
+
+    const data = undefined;
+
+    graph.addNode(a, data);
+    graph.addNode(b, data);
+    graph.addNode(c, data);
+
+    graph.addDependency(a, b);
+    graph.addDependency(b, c);
+    graph.addDependency(c, a);
+
+    expect(() => graph.dependenciesOf(b)).toThrowError('A cycle detected in the dependency graph');
+});
+
+test('resolving dependants in a graph that has cycle(s) throws', () => {
+    const graph = new Graph<string, void>();
+    const a = 'A';
+    const b = 'B';
+    const c = 'C';
+
+    const data = undefined;
+
+    graph.addNode(a, data);
+    graph.addNode(b, data);
+    graph.addNode(c, data);
+
+    graph.addDependency(a, b);
+    graph.addDependency(b, c);
+    graph.addDependency(c, a);
+
+    expect(() => graph.dependantsOf(b)).toThrowError('A cycle detected in the dependency graph');
+});
+
+test('overallOrder returns a legal execution path for graphs with disconnected sub graphs', () => {
+    const graph = new Graph<string, void>();
+
+    const a = 'A';
+    const b = 'B';
+    const c = 'C';
+    const d = 'D';
+    const e = 'E';
+    const f = 'F';
+    const g = 'G';
+    const h = 'H';
+    const i = 'I';
+    const j = 'J';
+    const k = 'K';
+    const l = 'L';
+    const m = 'M';
+
+    const data = undefined;
+
+    graph.addNode(a, data);
+    graph.addNode(b, data);
+    graph.addNode(c, data);
+    graph.addNode(d, data);
+    graph.addNode(e, data);
+    graph.addNode(f, data);
+    graph.addNode(g, data);
+    graph.addNode(h, data);
+    graph.addNode(i, data);
+    graph.addNode(j, data);
+    graph.addNode(k, data);
+    graph.addNode(l, data);
+    graph.addNode(m, data);
+
+    //      A       D   G I  /-J
+    //     / \     / \ /  | /  |
+    //    B   C   E --|   K    |
+    //     \   \ /    H   |    |
+    //      --- F         L -- M
+
+    graph.addDependency(a, b);
+    graph.addDependency(a, c);
+    graph.addDependency(b, f);
+    graph.addDependency(c, f);
+    graph.addDependency(d, e);
+    graph.addDependency(e, f);
+    graph.addDependency(d, h);
+    graph.addDependency(h, e);
+    graph.addDependency(g, h);
+    graph.addDependency(i, k);
+    graph.addDependency(k, l);
+    graph.addDependency(j, k);
+    graph.addDependency(j, m);
+    graph.addDependency(l, m);
+
+    expect(
+        Array.from(graph.overallOrder())
+    ).toEqual([f,b,c,a,e,h,d,g,m,l,k,i,j]);
+});
---
+++ src/dependency-graph.ts
@@ -0,0 +1,185 @@
+export class GraphError extends Error {
+
+}
+
+export class NoSuchNodeGraphError<K> extends GraphError {
+    public constructor(node: K) {
+        super(`Node '${node}' does not exist in graph`);
+    }
+}
+
+export class NoSuchEdgesGraphError<K> extends GraphError {
+    public constructor(node: K) {
+        super(`No edges found for node '${node}' in graph`);
+    }
+}
+
+export class GraphCycleError<K> extends GraphError {
+    public readonly cycle: K[];
+    public constructor(cycle: K[]) {
+        super('A cycle detected in the dependency graph');
+        this.cycle = cycle;
+    }
+
+    public toString(): string {
+        return this.message + `
+
+${this.cycle.join(' -> ')}`;
+    }
+}
+
+interface DFS<K> {
+    (node: K): void;
+}
+
+function createDfs<K>(edges: Map<K, Set<K>>, leavesOnly: boolean, result: Set<K>): DFS<K> {
+    const stack: K[] = [];
+    const visited: Set<K> = new Set();
+
+    const dfs = (currentNode: K): void => {
+        visited.add(currentNode);
+        stack.push(currentNode);
+
+        const currentEdges = edges.get(currentNode);
+        if (currentEdges == null) {
+            throw new NoSuchEdgesGraphError(currentNode);
+        }
+        
+        currentEdges.forEach((node) => {
+            if (!visited.has(node)) {
+                dfs(node);
+            } else if(stack.indexOf(node) > -1) {
+                stack.push(node);
+                throw new GraphCycleError(stack);
+            }
+        });
+
+        stack.pop();
+        
+        if ((!leavesOnly || currentEdges.size === 0) && !result.has(currentNode)) {
+            result.add(currentNode);
+        }
+    };
+
+    return dfs;
+}
+
+export class Graph<K, V> {
+
+    // This property is only protected for testing purposes.
+    protected incomingEdges: Map<K, Set<K>>;
+    // This property is only protected for testing purposes.
+    protected outgoingEdges: Map<K, Set<K>>;
+    // This property is only protected for testing purposes.
+    protected nodes: Map<K, V>;
+
+    public constructor() {
+        this.nodes = new Map;
+        this.incomingEdges = new Map;
+        this.outgoingEdges = new Map;
+    }
+
+    public addNode(node: K, data: V): void {
+        if (!this.nodes.has(node)) {
+            this.nodes.set(node, data);
+            
+            this.incomingEdges.set(node, new Set);
+            this.outgoingEdges.set(node, new Set);
+        }
+    }
+
+    public getNodeData(node: K): V {
+        const data = this.nodes.get(node);
+        if (data == null) {
+            throw new NoSuchNodeGraphError(node);
+        }
+        return data;
+    }
+
+    public setNodeData(id: K, data: V): void {
+        if (!this.nodes.has(id)) {
+            throw new NoSuchNodeGraphError(id);
+        }
+        this.nodes.set(id, data);
+    }
+
+    public hasNode(node: K): boolean {
+        return this.nodes.has(node);
+    }
+
+    public addDependency(from: K, to: K): void {
+        const outgoingEdges = this.outgoingEdges.get(from);
+        if (outgoingEdges == null) {
+            throw new NoSuchNodeGraphError(from);
+        }
+
+        const incomingEdges = this.incomingEdges.get(to);
+        if (incomingEdges == null) {
+            throw new NoSuchNodeGraphError(to);
+        }
+
+        incomingEdges.add(from);
+        outgoingEdges.add(to);
+    }
+
+    public removeDependency(from: K, to: K): void {
+        const outgoingEdges = this.outgoingEdges.get(from);
+        if (outgoingEdges != null) {
+            outgoingEdges.delete(to);
+        }
+
+        const incomingEdges = this.incomingEdges.get(to);
+        if (incomingEdges != null) {
+            incomingEdges.delete(from);
+        }
+    }
+
+    public dependenciesOf(node: K, leavesOnly: boolean = false): Set<K> {
+        if (this.nodes.has(node)) {
+            const result = new Set<K>();
+            const dfs = createDfs(this.outgoingEdges, leavesOnly, result);
+            
+            dfs(node);
+            result.delete(node);
+            
+            return result;
+        } else {
+            throw new NoSuchNodeGraphError(node);
+        }
+    }
+
+    public dependantsOf(node: K, leavesOnly: boolean = false): Set<K> {
+        if (this.nodes.has(node)) {
+            const result = new Set<K>();
+            const dfs = createDfs(this.incomingEdges, leavesOnly, result);
+            
+            dfs(node);
+            result.delete(node);
+            
+            return result;
+        } else {
+            throw new NoSuchNodeGraphError(node);
+        }
+    }
+
+    public overallOrder(leavesOnly: boolean = false) {
+        const result = new Set<K>();
+        if (this.nodes.size === 0) {
+            return result;
+        }
+        const keys = Array.from(this.nodes.keys());
+        const cycleDfs = createDfs(this.outgoingEdges, false, new Set<K>());
+        keys.forEach(cycleDfs);
+
+        const dfs = createDfs(this.outgoingEdges, leavesOnly, result);
+        keys.filter(node => {
+            const ie = this.incomingEdges.get(node);
+            if (ie == null) {
+                throw new GraphError('This should never happen');
+            }
+            return ie.size === 0;
+        }).forEach(dfs);
+
+        return result;
+    }
+}
