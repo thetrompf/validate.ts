23851895c5f68c7cdbb496cfc65daf8dcae00bbe
Brian K. Christensen <bkc@secoya.dk>
2017-02-22 13:58:01 +0100

    Examples: setup basic examples with dom helpers

--- examples/simple-form-validation-on-submit/simple-form.js
+++ examples/simple-form-validation-on-submit/simple-form.js
@@ -1,93 +1,104 @@
-/* global define, document */
-define(['require', 'validate'], (require, _validate) => {
-    const { validate, ValidationError } = _validate;
-    const form = document.querySelector('form');
-    const submitButton = document.querySelector('button');
-    if (!form || !submitButton) {
-        throw new Error('Couldn\'t find form or submit button');
-    }
+/* eslint-env browser */
+/* global define */
+/* eslint-disable import/no-amd */
 
-    const formFieldMap = {
-        name: document.getElementById('name'),
-        username: document.getElementById('username'),
-    };
+define(['require', '/dist/amd/validate.js'], (require) => {
 
-    const formGroupMap = {
-        name: document.querySelector('.form-group.name'),
-        username: document.querySelector('.form-group.username'),
-    };
+    /* eslint-disable import/no-dynamic-require */
+    require(['validate', 'validation/dom'], (_validate, validateDom) => {
+        const { validate, ValidationError } = _validate;
+        const { getFormValidationObject } = validateDom;
 
-    const formFeedbackMap = {
-        name: document.querySelector('.form-control-feedback.name'),
-        username: document.querySelector('.form-control-feedback.username'),
-    };
-
-    const clearError = (field) => {
-        formGroupMap[field].classList = `form-group ${field}`;
-        formFeedbackMap[field].innerHTML = '';
-    };
-
-    const clearErrors = () => {
-        clearError('name');
-        clearError('username');
-    };
-
-    const addErrorsToField = (field, errors) => {
-        formGroupMap[field].classList = `form-group has-danger ${field}`;
-        if (errors.length > 1) {
-            formFeedbackMap[field].innerHTML = '<ul><li>' + errors.map(e => e.message).join('</li><li>') + '</li></ul>';
-        } else if (errors.length === 1) {
-            formFeedbackMap[field].innerHTML = errors[0].message;
+        const form = document.querySelector('form');
+        const submitButton = document.querySelector('button');
+        if (!form || !submitButton) {
+            throw new Error('Couldn\'t find form or submit button');
         }
-    };
 
-    const addErrors = (errors) => {
-        for (const fieldErrors of Array.from(errors.errors)) {
-            addErrorsToField(fieldErrors[0], fieldErrors[1]);
-        }
-    };
+        const formValidationObject = getFormValidationObject(form, {
+            feedbackSelector: '.form-control-feedback',
+            fieldSelector: '.form-group',
+            editorSelector: '[name]',
+            errorClass: 'has-danger',
+        });
 
-    submitButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        event.stopPropagation();
+        submitButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            event.stopPropagation();
 
-        clearErrors();
+            formValidationObject.clearErrors();
 
-        validate(
-            {
-                name: formFieldMap.name.value,
-                username: formFieldMap.username.value,
-            }, {
-                name: {
-                    validators: [
-                        (value) => {
-                            if (value.length <= 5) {
-                                return Promise.reject(new ValidationError('Name must contain 5 chars or more'));
-                            }
-                            return Promise.resolve(null);
-                        },
-                    ],
-                },
-                username: {
-                    validators: [
-                        (value) => {
-                            if (value.length < 7) {
-                                return Promise.reject(new ValidationError('Username must contain 7 chars or more'));
-                            }
-                            return Promise.resolve(null);
-                        },
-                        (value) => {
-                            if (value.indexOf('@') === -1 || value.indexOf('.') === -1) {
-                                // poor man's email validator.
-                                return Promise.reject(new ValidationError('Username must be a valid email address'));
-                            }
-                            return Promise.resolve(null);
-                        },
-                    ],
-                },
-            }
-        ).catch((e) => {
-            addErrors(e);
+            validate(
+                formValidationObject.values,
+                {
+                    'name': {
+                        validators: [
+                            (value) => {
+                                if (value.length <= 5) {
+                                    return Promise.reject(new ValidationError('Name must contain 5 chars or more'));
+                                }
+                                return Promise.resolve(null);
+                            },
+                        ],
+                    },
+                    'username': {
+                        validators: [
+                            (value) => {
+                                if (value.length < 7) {
+                                    return Promise.reject(new ValidationError('Username must contain 7 chars or more'));
+                                }
+                                return Promise.resolve(null);
+                            },
+                            (value) => {
+                                if (value.indexOf('@') === -1 || value.indexOf('.') === -1) {
+                                    // poor man's email validator.
+                                    return Promise.reject(new ValidationError('Username must be a valid email address'));
+                                }
+                                return Promise.resolve(null);
+                            },
+                        ],
+                    },
+                    'password': {
+                        validators: [
+                            (value) => {
+                                if (value.length < 8) {
+                                    return Promise.reject(new ValidationError(`Password is ${8 - value.length + 1} char(s) too short.`));
+                                }
+                                return Promise.resolve(null);
+                            },
+                            (value) => {
+                                if (!/[a-z]/.test(value)) {
+                                    return Promise.reject(new ValidationError('Password must contain lower case letters'));
+                                }
+                                return Promise.resolve(null);
+                            },
+                            (value) => {
+                                if (!/[A-Z]/.test(value)) {
+                                    return Promise.reject(new ValidationError('Password must contain upper case letters'));
+                                }
+                                return Promise.resolve(null);
+                            },
+                            (value) => {
+                                if (!/[0-9]/.test(value)) {
+                                    return Promise.reject(new ValidationError('Password must contain numbers'));
+                                }
+                                return Promise.resolve(null);
+                            },
+                        ],
+                    },
+                    'password-repeat': {
+                        dependencies: ['password'],
+                        validators: [
+                            (value, dependencies) => {
+                                if (value !== dependencies.get('password')) {
+                                    return Promise.reject(new ValidationError('Must match the other password field'));
+                                }
+                                return Promise.resolve(null);
+                            },
+                        ],
+                    },
+                }
+            ).catch(formValidationObject.setErrors);
         });
     });
 });
---
+++ src/validation/dom.ts
@@ -0,0 +1,108 @@
+import {
+    ValidationAggregateError,
+} from 'validation/errors';
+
+import {
+    Constraints,
+} from 'validation/types';
+
+export interface WrapOptions {
+    feedbackSelector: string | null;
+    fieldSelector: string;
+    editorSelector: string;
+    errorClass: string;
+}
+
+const defaultOptions: WrapOptions = {
+    feedbackSelector: null, // '.validation-feedback',
+    fieldSelector: '.form-field',
+    editorSelector: '[name]',
+    errorClass: 'has-errors',
+};
+
+export interface FormMap {
+    [fieldName: string]: {
+        fieldElement: Element,
+        editorElement: Element | null,
+        feedbackElement: Element | null,
+    };
+}
+
+function createClearErrorsFn(formMap: FormMap, options: WrapOptions) {
+    return () => {
+        for (const field of Object.keys(formMap)) {
+            const map = formMap[field];
+            if (map.feedbackElement) {
+                map.feedbackElement.innerHTML = '';
+            }
+            map.fieldElement.classList.remove(options.errorClass);
+        }
+    };
+}
+
+function createSetErrorsFn(formMap: FormMap, options: WrapOptions) {
+    return (errors: ValidationAggregateError<FormMap>) => {
+        errors.errors.forEach((validationErrors, field) => {
+            formMap[field].fieldElement.classList.add(options.errorClass);
+            const feedbackElement = formMap[field].feedbackElement;
+            if (feedbackElement != null) {
+                if (validationErrors.length > 1) {
+                    feedbackElement.innerHTML = `
+                        <ul>
+                            <li>${validationErrors.map(e => e.message).join('</li><li>')}</li>
+                        </ul>
+                    `;
+                } else {
+                    feedbackElement.innerHTML = validationErrors[0].message as string;
+                }
+            }
+        });
+    };
+}
+
+export function getFormValidationObject(form: HTMLFormElement, options?: Partial<WrapOptions>) {
+    const resolvedOptions = Object.assign({}, defaultOptions, (options || {}));
+    const fieldElements = form.querySelectorAll(resolvedOptions.fieldSelector);
+    if (fieldElements.length === 0) {
+        throw new Error(`No form fields found with selector: ${resolvedOptions.fieldSelector}`);
+    }
+
+    const formMap: FormMap = {};
+    const values = {} as any;
+    for (const fieldElement of Array.from(fieldElements)) {
+        const editorElement = fieldElement.querySelector(resolvedOptions.editorSelector);
+        if (editorElement == null) {
+            throw new Error(`Couldn't find editor element for ${fieldElement}`);
+        }
+        const fieldName = editorElement.getAttribute('name') as string;
+        formMap[fieldName] = {
+            fieldElement: fieldElement,
+            editorElement: editorElement,
+            feedbackElement: (
+                resolvedOptions.feedbackSelector == null
+                    ? null
+                    : fieldElement.querySelector(resolvedOptions.feedbackSelector)
+            ),
+        };
+    }
+
+    const result = {
+        clearErrors: createClearErrorsFn(formMap, resolvedOptions),
+        formMap: formMap,
+        setErrors: createSetErrorsFn(formMap, resolvedOptions),
+    };
+
+    Object.defineProperty(result, 'values', {
+        enumerable: true,
+        configurable: false,
+        get: () => {
+            const res = {} as any;
+            for (const field of Object.keys(formMap)) {
+                res[field] = (formMap[field].editorElement as HTMLInputElement).value;
+            }
+            return res;
+        }
+    });
+
+    return result;
+}
